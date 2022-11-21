b32e2cb567875e6400e98a4ab325d2b9a5aa4aea
Naya <44099724+nayamairena@users.noreply.github.com>
2022-11-20 22:53:46 -0800

    adding routes for front end

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
