1ad475ca832c78349e21bccdd5c1276022ec608d
Naya <44099724+nayamairena@users.noreply.github.com>
2022-11-21 17:08:36 -0800

    adding hooks and submit button for log in form

--- client/src/components/FormBox.js
+++ client/src/components/FormBox.js
@@ -4,19 +4,41 @@ import Tab from 'react-bootstrap/Tab';
 
 const FormBox = () => {
   const [key, setKey] = useState('Login');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmitLogin = (event) => {
+    event.preventDefault();
+    console.log(username);
+    //I know this is bad but just for now lol.
+    console.log(password);
+    //Do back end log in stuff?
+  };
+
   return (
     <Tabs
       id='controlled-tab'
       activeKey={key}
       onSelect={(temp) => setKey(temp)}
-      className='mb-3'
+      className='m-5'
     >
       <Tab eventKey='Login' title='Login'>
-        <form>
+        <form onSubmit={handleSubmitLogin}>
           <label className='form-label'>Username</label>
-          <input type='email' className='form-control' />
+          <input
+            type='text'
+            className='form-control m-5'
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
           <label className='form-label'>Password</label>
-          <input type='password' className='form-control' />
+          <input
+            type='password'
+            className='form-control'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <button type='submit'>Log In</button>
         </form>
       </Tab>
       <Tab eventKey='Register' title='Register'>
