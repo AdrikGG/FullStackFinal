a4fd3604bc481d33147fb7b6df9114e9bfbd8a41
Stoic_pdx <bkoehler2019@gmail.com>
2022-11-23 14:58:20 -0800

    More add

--- client/src/silo_game/scoreCard.jsx
+++ client/src/silo_game/scoreCard.jsx
@@ -1,15 +1,16 @@
-import Card from 'bootstrap-react/Card';
+import Card from 'react-bootstrap/Card';
 import scoreBoard from './game';
 
-function scoreCard() {
+function ScoreCard() {
+    let score = scoreBoard();
     return(
         <Card className="text-center" style={{ width: '20rem'}}>
             <Card.body>
                 <Card.Title>Current Score</Card.Title>
-                <Card.Text></Card.Text>
+                <Card.Text>{score}</Card.Text>
             </Card.body>
         </Card>
-    )
+    );
 }
 
-export default scoreCard;
+export default ScoreCard;
