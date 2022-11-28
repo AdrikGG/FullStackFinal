00f8fa85e9f021487d653927e61d07fc0cfdcc54
Naya <44099724+nayamairena@users.noreply.github.com>
2022-11-28 01:33:32 -0800

    basic account page layout almost done

--- client/src/components/ProfileBox.js
+++ client/src/components/ProfileBox.js
@@ -1,40 +1,75 @@
 import React, { useState } from 'react';
-
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
+import cat from './cat.jpg';
 import { FaUserCircle } from 'react-icons/fa';
-import MainButton from './MainButton';
 
 const ProfileBox = () => {
-  let avatar = false;
-  if (avatar) {
-  }
+  const displayAvi = (hasAvi) => {
+    let profilePic = '';
+    if (hasAvi) {
+      //for now the profile pic will be of a cat, will need to figure out back end stuff to allow user uploading.
+      profilePic = (
+        <Image
+          width={180}
+          height={180}
+          roundedCircle={true}
+          src={cat}
+          alt='no'
+        ></Image>
+      );
+    } else {
+      profilePic = (
+        <FaUserCircle
+          size={180}
+          className='m-3'
+          style={{ color: '#6d8fae ' }}
+        ></FaUserCircle>
+      );
+    }
+    return profilePic;
+  };
+
+  //have a function to check if account has an avi from back end
+  //for now it will be false bc we want to show default icon for no avi
+  let pix = displayAvi(false);
 
   return (
-    <Container className='mt-5 pt-5 bg-light border rounded w-50'>
+    <Container className='mt-5 pt-3 bg-light border rounded w-50'>
       <Row>
-        <FaUserCircle></FaUserCircle>
+        <Col className='d-flex justify-content-center'>{pix}</Col>
       </Row>
-      <Row>
-        <Col>Username:</Col>
-        <Col>Nayapapaya</Col>
+      <Row className='my-3'>
+        <Col className='d-flex justify-content-around fw-bold'>Username:</Col>
+        <Col className='d-flex justify-content-around text-primary'>
+          Nayapapaya
+        </Col>
       </Row>
-      <Row>
-        <Col>Email: </Col>
-        <Col>mnaya@pdx.edu</Col>
+      <Row className='my-3'>
+        <Col className='d-flex justify-content-around fw-bold'>Email:</Col>
+        <Col className='d-flex justify-content-around  text-primary'>
+          mnaya@pdx.edu
+        </Col>
       </Row>
-      <Row>
-        <Col>High Score Game1</Col>
-        <Col>High Score Game2</Col>
-      </Row>
-      <Row>
-        <Col>100</Col>
-        <Col>200</Col>
-      </Row>
-      <Row>
-        <MainButton text='Edit Profile' type='button' />
+      <Row className='border m-2'>
+        <Row className='m-2'>
+          <Col className='d-flex justify-content-around fw-bold'>
+            High Score Game1
+          </Col>
+          <Col className='d-flex justify-content-around  text-primary'>
+            1000
+          </Col>
+        </Row>
+        <Row className='m-2'>
+          <Col className='d-flex justify-content-around fw-bold'>
+            High Score Game2
+          </Col>
+          <Col className='d-flex justify-content-around  text-primary'>
+            2000
+          </Col>
+        </Row>
       </Row>
     </Container>
   );
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,15 @@
 import ProfileBox from '../components/ProfileBox';
+import MainButton from '../components/MainButton';
+import EasyEdit, { Types } from 'react-easy-edit';
+import Row from 'react-bootstrap/Row';
+
 const Profile = () => {
   return (
     <div className='p-5'>
-      <ProfileBox></ProfileBox>
+      <Row>
+        <ProfileBox></ProfileBox>
+        <MainButton text='Edit Profile' type='button' />
+      </Row>
     </div>
   );
 };
