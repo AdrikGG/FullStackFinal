5f1139175142a2ee505c7a12440e697e7772d77f
Naya <44099724+nayamairena@users.noreply.github.com>
2022-11-29 14:02:52 -0800

    Fix width properties on flexbox

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,15 +7,13 @@ const Home = () => {
     <>
       <h1>Home page</h1>
 
-      <Row className='m-2 flex-wrap'>
-        <Col className='m-2'>
+      <Row className='m-3 flex-wrap justify-content-center'>
+        <Col className='m-3' style={{ maxWidth: 550 }}>
           <p className={'text-center fw-bold font-monospace fs-2'}>Game 1</p>
-
           <PlayGame customClass={'gameOne'} isGame1={true}></PlayGame>
         </Col>
-        <Col className='m-2'>
+        <Col className='m-3' style={{ maxWidth: 550 }}>
           <p className={'text-center fw-bold font-monospace fs-2'}>Game 2</p>
-
           <PlayGame customClass={'gameTwo'} isGame2={true}></PlayGame>
         </Col>
       </Row>
