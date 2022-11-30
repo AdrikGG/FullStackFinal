a74f2f79810632f83d2d5e21dbe6d32895549f4f
Adrik Gurganus <adrik@pdx.edu>
2022-11-30 14:07:49 -0800

    partial heroku deployment setup

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -16,6 +17,18 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(bodyParser.json({ limit: '10mb' }));
 
 app.use('/api/users/', userRoutes);
+
+// -------- Deployment --------
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// -------- Deployment --------
 
 mongoose
   .connect(process.env.DB_URL, {
