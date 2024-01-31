e814fce9f32628f2d9cabb9e1d9a5429ec665c02
Adrik Gurganus <adrik@pdx.edu>
2024-01-31 07:25:45 -0800

    removed line pointing to nonexistent frontend build

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/users');
 const gameRoutes = require('./routes/games');
-const path = require('path');
 
 require('dotenv').config();
 
@@ -19,19 +18,6 @@ app.use(bodyParser.json({ limit: '10mb' }));
 
 app.use('/api/users/', userRoutes);
 app.use('/api/games/', gameRoutes);
-
-// -------- Deployment --------
-
-__dirname = path.resolve();
-console.log(__dirname);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-// -------- Deployment --------
 
 mongoose
   .connect(process.env.DB_URL, {
